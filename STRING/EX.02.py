40f07b3653b7a4fe3c545344b774fd9a7c85d871
Capovillaa <160801041+Capovillaa@users.noreply.github.com>
2024-04-18 13:30:44 -0300

    Delete STRING directory

--- STRING/EX.02.py
+++
@@ -1,4 +0,0 @@
-'''Faça um programa que leia um nome e imprima as 4
-primeiras letras do nome'''
-nome=input("digite seu nome:")
-print(nome[0:4])
--- STRING/EX.06.py
+++
@@ -1,6 +0,0 @@
-S=input('Digite uma frase:')
-Semespaço=(S.replace(" ",""))#defini essa variavel para que seja possivel verificar um palindromo em frases pois se contar o espaço obviamente n funciona
-if(Semespaço.lower() == Semespaço.lower()[::-1]):#aqui pode ser == ou IN tbm funcionaria
-    print(f"A string {S.title()} é um Palíndromo")
-else:
-    print("Não é um palíndromo")
